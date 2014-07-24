f3d5a9a740898922417c3adbbe5c4ecc159c7765
Melanie Fryman <melanie@frymanet.com>
2014-07-24 16:02:48 -0500

    added cost function

--- app/models/apartment.js
+++ app/models/apartment.js
@@ -1,9 +1,22 @@
 'use strict';
+//var Room = require('./room.js');
 
 function Apartment(unit){
   this.unit = unit;
   this.rooms = [];
   this.renters = [];
 }
+
+Apartment.prototype.area = function(){
+  var total = 0;
+  for(var i = 0; i < this.rooms.length; i++){
+    total += (this.rooms[i].length * this.rooms[i].width);
+  }
+  return total;
+};
+
+Apartment.prototype.cost = function(){
+  return this.area() * 5;
+};
 
 module.exports = Apartment;
--- test/unit/apartment.js
+++ test/unit/apartment.js
@@ -5,7 +5,7 @@
 
 var expect = require('chai').expect;
 var Apartment = require('../../app/models/apartment.js');
-//var Room = require('../../app/models/room.js');
+var Room = require('../../app/models/room.js');
 //var Renter = require('../../app/models/renter.js');
 
 describe('Apartment', function(){
@@ -17,6 +17,34 @@ describe('Apartment', function(){
       expect(a1.unit).to.equal('A1');
       expect(a1.rooms).to.have.length(0);
       expect(a1.renters).to.have.length(0);
+    });
+  });
+  
+  describe('#area', function(){
+    it('should calculate the area of the apartment', function(){
+      var a1 = new Apartment('A1');
+      var br1 = new Room('bedroom', '50', '10');
+      var br2 = new Room('bedroom', '75', '30');
+      var br3 = new Room('bedroom', '30', '15');
+      var ba1 = new Room('bathroom', '10', '15');
+      var ba2 = new Room('bathroom', '25', '12');
+      a1.rooms.push(br1, br2, br3, ba1, ba2);
+
+      expect(a1.rooms).to.have.length(5);
+      expect(a1.area()).to.equal(3650);
+    });
+  });
+  describe('#cost', function(){
+    it('should calculate the cost of the apartment', function(){
+      var a1 = new Apartment('A1');
+      var br1 = new Room('bedroom', '50', '10');
+      var br2 = new Room('bedroom', '75', '30');
+      var br3 = new Room('bedroom', '30', '15');
+      var ba1 = new Room('bathroom', '10', '15');
+      var ba2 = new Room('bathroom', '25', '12');
+      a1.rooms.push(br1, br2, br3, ba1, ba2);
+     
+      expect(a1.cost()).to.equal(18250);
     });
   });
 });
