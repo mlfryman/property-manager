2ba1ae6f181cda3506665c5269c6ec73f75aedb7
Melanie Fryman <melanie@frymanet.com>
2014-07-25 12:30:19 -0500

    added ALL the things! (#save, .find, .findById, .deleteById)

--- app/models/apartment.js
+++ app/models/apartment.js
@@ -1,7 +1,10 @@
 'use strict';
 
-//var Room = require('./room');
-//var Renter = require('./renter');
+var cApartment = global.mongodb.collection('apartments');
+var _ = require('lodash');
+
+var Room = require('./room');
+var Renter = require('./renter');
 
 function Apartment(unit){
   this.unit = unit;
@@ -57,5 +60,51 @@ Apartment.prototype.collectRent = function(){
     this.renters[i].payRent(rent);
   }
 };
+
+Apartment.prototype.save = function(cb){
+  cApartment.save(this, function(err, obj){
+    cb();
+  });
+};
+
+Apartment.find = function(query, cb){
+  cApartment.find(query).toArray(function(err, apts){
+    for(var i = 0; i < apts.length; i++){
+      apts[i] = reProto(apts[i]);
+    }
+    cb(apts);
+  });
+};
+
+Apartment.findById = function(id, cb){
+  var query = {_id:id};
+  cApartment.findOne(query, function(err, apt){
+    cb(reProto(apt));
+  });
+};
+
+Apartment.deleteById = function(id, cb){
+  var query = {_id:id};
+  cApartment.remove(query, function(){
+    cb();
+  });
+};
+
+// HELPER FUNCTIONS //
+
+function reProto(apt){
+  var room, renter;
+  for(var i = 0; i < apt.rooms.length; i++){
+    room = _.create(Room.prototype, apt.rooms[i]);
+    apt.rooms[i] = room;
+  }
+  for(var j = 0; j < apt.renters.length; j++){
+    renter = _.create(Renter.prototype, apt.renters[j]);
+    apt.renters[j] = renter;
+  }
+  apt = _.create(Apartment.prototype, apt);
+
+  return apt;
+}
 
 module.exports = Apartment;
